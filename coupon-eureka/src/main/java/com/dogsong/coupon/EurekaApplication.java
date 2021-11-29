eee3039857be385635ad8859e55c37e4f755bfea
domi.song <domi.song@yunzhihui.com>
2021-11-29 17:01:52 +0800

    Add Gateway Server

--- coupon-eureka/src/main/java/com/dogsong/coupon/EurekaApplication.java
+++ coupon-eureka/src/main/java/com/dogsong/coupon/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.dogsong;
+package com.dogsong.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ coupon-gateway/src/main/java/com/dogsong/coupon/ZuulGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.dogsong.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * <h1>网关应用启动入口</h1>
+ * 1. @EnableZuulProxy 标识当前的应用是 Zuul Server
+ * 2. @SpringCloudApplication 组合了 SpringBoot 应用 + 服务发现 + 熔断
+ *
+ * @author <a href="mailto:dogsong99@gmail.com">domi</a>
+ * @since 2021/11/29
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
