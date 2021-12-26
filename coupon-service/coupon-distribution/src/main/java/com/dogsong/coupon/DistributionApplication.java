0860d55ea1d49464cc11feb501d4c560491cdd22
dogsong <dogsong99@gmail.com>
2021-12-26 14:49:59 +0800

    创建优惠券分发微服务2

--- coupon-service/coupon-distribution/src/main/java/com/dogsong/coupon/DistributionApplication.java
+++ coupon-service/coupon-distribution/src/main/java/com/dogsong/coupon/DistributionApplication.java
@@ -3,9 +3,12 @@ package com.dogsong.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 分发系统启动入口
@@ -19,6 +22,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableEurekaClient
 @SpringBootApplication
 public class DistributionApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DistributionApplication.class, args);
