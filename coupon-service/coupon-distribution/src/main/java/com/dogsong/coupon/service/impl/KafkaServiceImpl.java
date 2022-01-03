2374363edbb5e597244c26c7cdc1a9303b4dfcea
dogsong <dogsong99@gmail.com>
2022-01-03 22:52:57 +0800

    Kafka 消费消息逻辑

--- coupon-service/coupon-distribution/src/main/java/com/dogsong/coupon/service/impl/KafkaServiceImpl.java
+++ coupon-service/coupon-distribution/src/main/java/com/dogsong/coupon/service/impl/KafkaServiceImpl.java
@@ -3,13 +3,18 @@ package com.dogsong.coupon.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.dogsong.coupon.constant.Constant;
 import com.dogsong.coupon.constant.CouponStatus;
+import com.dogsong.coupon.dao.CouponDao;
+import com.dogsong.coupon.entity.Coupon;
 import com.dogsong.coupon.service.IKafkaService;
 import com.dogsong.coupon.vo.CouponKafkaMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -21,6 +26,13 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class KafkaServiceImpl implements IKafkaService {
+
+    private final CouponDao couponDao;
+
+    @Autowired
+    public KafkaServiceImpl(CouponDao couponDao) {
+        this.couponDao = couponDao;
+    }
 
     @Override
     @KafkaListener(topics = {Constant.TOPIC}, groupId = "coupon-1")
@@ -39,11 +51,44 @@ public class KafkaServiceImpl implements IKafkaService {
                 case USABLE:
                     break;
                 case USED:
+                    processUsedCoupons(couponInfo, status);
                     break;
                 case EXPIRED:
+                    processExpiredCoupons(couponInfo, status);
                     break;
             }
         }
     }
 
+    /**
+     * 处理已使用的优惠券
+     */
+    private void processUsedCoupons(CouponKafkaMessage kafkaMessage, CouponStatus status) {
+        // todo 给用户发送信息
+        processCouponByStatus(kafkaMessage, status);
+    }
+
+    /**
+     * 处理过期的优惠券
+     */
+    private void processExpiredCoupons(CouponKafkaMessage kafkaMessage, CouponStatus status) {
+        // todo 给用户发送信息
+        processCouponByStatus(kafkaMessage, status);
+    }
+
+    /**
+     * 根据状态处理优惠券信息
+     */
+    private void processCouponByStatus(CouponKafkaMessage kafkaMessage, CouponStatus status) {
+        List<Coupon> coupons = couponDao.findAllById(kafkaMessage.getIds());
+
+        if (CollectionUtils.isEmpty(coupons) || coupons.size() != kafkaMessage.getIds().size()) {
+            log.error("Can Not Find Right Coupon Info: {}.", JSON.toJSONString(kafkaMessage));
+            // todo 发送邮件
+            return;
+        }
+
+        coupons.forEach(c -> c.setStatus(status));
+        log.info("CouponKafkaMessage Op Coupon Count: {}.", couponDao.saveAll(coupons).size());
+    }
 }
