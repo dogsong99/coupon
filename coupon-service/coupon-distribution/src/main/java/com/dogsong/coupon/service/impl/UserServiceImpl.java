3402be3708f538017b220eb48a358b88e6ecdb0c
dogsong <dogsong99@gmail.com>
2022-01-31 11:04:21 +0800

    查询优惠券记录

---
+++ coupon-service/coupon-distribution/src/main/java/com/dogsong/coupon/service/impl/UserServiceImpl.java
@@ -0,0 +1,179 @@
+package com.dogsong.coupon.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.dogsong.coupon.constant.Constant;
+import com.dogsong.coupon.constant.CouponStatus;
+import com.dogsong.coupon.dao.CouponDao;
+import com.dogsong.coupon.entity.Coupon;
+import com.dogsong.coupon.exception.CouponException;
+import com.dogsong.coupon.feign.SettlementClient;
+import com.dogsong.coupon.feign.TemplateClient;
+import com.dogsong.coupon.service.IRedisService;
+import com.dogsong.coupon.service.IUserService;
+import com.dogsong.coupon.vo.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 用户服务相关的接口实现
+ *
+ * 所有的操作过程, 状态都保存在 Redis 中, 并通过 Kafka 把消息传递到 MySQL 中
+ * 为什么使用 Kafka, 而不是直接使用 SpringBoot 中的异步处理 ?
+ *
+ * @author <a href="mailto:dogsong99@gmail.com">dogsong</a>
+ * @since 2022/1/30
+ */
+@Slf4j
+@Service
+public class UserServiceImpl implements IUserService {
+
+    /** Coupon Dao */
+    private final CouponDao couponDao;
+
+    /** Redis 服务 */
+    private final IRedisService redisService;
+
+    /** 模板微服务客户端 */
+    private final TemplateClient templateClient;
+
+    /** 结算微服务客户端 */
+    private final SettlementClient settlementClient;
+
+    /** Kafka 客户端 */
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public UserServiceImpl(CouponDao couponDao, IRedisService redisService,
+                           TemplateClient templateClient,
+                           SettlementClient settlementClient,
+                           KafkaTemplate<String, String> kafkaTemplate) {
+        this.couponDao = couponDao;
+        this.redisService = redisService;
+        this.templateClient = templateClient;
+        this.settlementClient = settlementClient;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+    /**
+     * 根据用户 id 和状态查询优惠券记录
+     *
+     * @param userId 用户 id
+     * @param status 优惠券状态
+     * @return {@link Coupon}s
+     */
+    @Override
+    public List<Coupon> findCouponsByStatus(Long userId, Integer status) throws CouponException {
+        List<Coupon> curCached = redisService.getCachedCoupons(userId, status);
+        List<Coupon> preTarget;
+
+        if (CollectionUtils.isNotEmpty(curCached)) {
+            log.debug("coupon cache is not empty: {}, {}", userId, status);
+            preTarget = curCached;
+        } else {
+            log.debug("coupon cache is empty, get coupon from db: {}, {}",
+                    userId, status);
+            List<Coupon> dbCoupons = couponDao.findAllByUserIdAndStatus(
+                    userId, CouponStatus.of(status)
+            );
+            // 如果数据库中没有记录, 直接返回就可以, Cache 中已经加入了一张无效的优惠券
+            if (CollectionUtils.isEmpty(dbCoupons)) {
+                log.debug("current user do not have coupon: {}, {}", userId, status);
+                return dbCoupons;
+            }
+
+            // 填充 dbCoupons的 templateSDK 字段
+            Map<Integer, CouponTemplateSDK> id2TemplateSDK =
+                    templateClient.findIds2TemplateSDK(
+                            dbCoupons.stream()
+                                    .map(Coupon::getTemplateId)
+                                    .collect(Collectors.toList())
+                    ).getData();
+            dbCoupons.forEach(
+                    dc -> dc.setTemplateSDK(
+                            id2TemplateSDK.get(dc.getTemplateId())
+                    )
+            );
+            // 数据库中存在记录
+            preTarget = dbCoupons;
+            // 将记录写入 Cache
+            redisService.addCouponToCache(userId, preTarget, status);
+        }
+
+        // 将无效优惠券剔除
+        preTarget = preTarget.stream()
+                .filter(c -> c.getId() != -1)
+                .collect(Collectors.toList());
+        // 如果当前获取的是可用优惠券, 还需要做对已过期优惠券的延迟处理
+        if (CouponStatus.of(status) == CouponStatus.USABLE) {
+            CouponClassify classify = CouponClassify.classify(preTarget);
+            // 如果已过期状态不为空, 需要做延迟处理
+            if (CollectionUtils.isNotEmpty(classify.getExpired())) {
+                log.info("Add Expired Coupons To Cache From FindCouponsByStatus: {}, {}", userId, status);
+                redisService.addCouponToCache(
+                        userId, classify.getExpired(),
+                        CouponStatus.EXPIRED.getCode()
+                );
+                // 发送到 kafka 中做异步处理
+                kafkaTemplate.send(
+                        Constant.TOPIC,
+                        JSON.toJSONString(new CouponKafkaMessage(
+                                CouponStatus.EXPIRED.getCode(),
+                                classify.getExpired().stream()
+                                        .map(Coupon::getId)
+                                        .collect(Collectors.toList())
+                        ))
+                );
+            }
+
+            return classify.getUsable();
+        }
+
+        return preTarget;
+    }
+
+    /**
+     * 根据用户 id 查找当前可以领取的优惠券模版
+     *
+     * @param userId 用户id
+     * @return {@link CouponTemplateSDK}s
+     */
+    @Override
+    public List<CouponTemplateSDK> findAvailableTemplate(Long userId) throws CouponException {
+        long curTime = new Date().getTime();
+
+
+
+        return null;
+    }
+
+    /**
+     * 用户领取优惠券
+     *
+     * @param request {@link AcquireTemplateRequest}
+     * @return {@link Coupon}
+     */
+    @Override
+    public Coupon acquireTemplate(AcquireTemplateRequest request) throws CouponException {
+        return null;
+    }
+
+    /**
+     * 结算(核销)优惠券
+     *
+     * @param info {@link SettlementInfo}
+     * @return {@link SettlementInfo}
+     */
+    @Override
+    public SettlementInfo settlement(SettlementInfo info) throws CouponException {
+        return null;
+    }
+}
